44833cb74d91cb140ad5ef87e83617371272040c
Altaf Hussain <altafsky17@gmail.com>
2023-11-01 10:09:15 +0530

    password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Counter from './Components/Hooks/01-useState/Counter'
+import PasswordGenerator from './Components/Hooks/03-useEffect-useCallback/PasswordGenerator'
 import BGChanger from './Components/Projects/BGChanger'
 import Props from './Components/Props & Tailwind/Props'
 
@@ -14,6 +15,7 @@ function App() {
       {/* <Counter /> */}
       {/* <Props /> */}
       {/* <BGChanger /> */}
+      <PasswordGenerator />
     </>
   )
 }
